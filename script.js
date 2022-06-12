b2e970a0baf58f4c7d93875b27329ff9396628d8
sennmeister <106446545+sennmeister@users.noreply.github.com>
2022-06-12 14:04:12 +0200

    start respoNav

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+
+const listContainer = document.getElementsByClassName("list-container")[0]
+
+
+
+toggleButton.addEventListener("click", () => {
+
+    listContainer.classList.toggle("active")
+
+})
